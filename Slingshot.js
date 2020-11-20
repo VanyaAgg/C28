9c5fd1b59a9382b8041a2891b6c02b41d13a6b10
VanyaAgg <shivaniagg@hotmail.com>
2020-11-20 19:23:12 +0530

    c28

---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+        console.log(this.sling)
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+        // null means nothing
+        //null also equates to false
+    }
+
+    display(){
+       if(this.sling.bodyA != null){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+       }
+    }
+    
+}
